7546b59942a70504ec04f4109028cc8a3741c309
Khijbar Qayoumi <khijbar@gmail.com>
2016-04-14 23:56:49 +0200

    about to start chapter 7.3.4 - Test for invalid subsmission

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
+  resources :users
+
   get 'users/new'
 
-  root 'static_pages#home'
-  get 'help' => 'static_pages#help'
-  get 'about' => 'static_pages#about'
-  get 'contact' => 'static_pages#contact'
-  get 'signup' => 'static_pages#signup'
+  root              'static_pages#home'
+  get 'help' =>     'static_pages#help'
+  get 'about' =>    'static_pages#about'
+  get 'contact' =>  'static_pages#contact'
+  get 'signup' =>   'users#new'
 
 end
