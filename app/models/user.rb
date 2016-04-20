36496aff33602adec959f0eb7c46e7618ec271e1
Khijbar Qayoumi <khijbar@gmail.com>
2016-04-20 16:42:00 +0200

    Finish log-in/log-out

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,7 +14,7 @@ class User < ActiveRecord::Base
   # returns the hash digest of the given string
   def User.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
-                                                  BCrypt::Engine.cost
+                                                   BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
   end
 
@@ -23,9 +23,21 @@ class User < ActiveRecord::Base
     SecureRandom.urlsafe_base64
   end
 
+  # remembers a user in the database for use in persisten sessions
   def remember
     self.remember_token = User.new_token
     update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
+  # returns true if the given token matches the digest
+  def authenticated?(remember_token)
+    return false if remember_digest.nil?
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  # forgets a user
+  def forget
+    update_attribute(:remember_digest, nil)
   end
 end
 
