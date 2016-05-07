eee30e5c953ffbaae8cc795d167cfdcebbdd9e29
Khijbar Qayoumi <khijbar@gmail.com>
2016-05-07 21:11:37 +0200

    End of the book

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
-  resources :users
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
   resources :account_activations, only: [:edit]
   resources :password_resets,     only: [:new, :create, :edit, :update]
   resources :microposts,          only: [:create, :destroy]
+  resources :relationships,       only: [:create, :destroy]
 
   root                'static_pages#home'
   get 'help' =>       'static_pages#help'
